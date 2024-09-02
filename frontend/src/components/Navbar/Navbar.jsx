a6490a655c6b2afe358b80115a19f8f44d8015bd
adityasrivastav5 <adityasrivastav863@gmail.com>
2024-09-02 23:11:37 +0530

    Some changes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,42 +1,42 @@
 import './Navbar.css';
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="logo">
-                <a href="#">LOGO</a>
+                <Link to="#">LOGO</Link>
             </div>
             <ul className="nav-links">
                 <li className="nav-item">
-                    <a href="#">
+                    <Link to="#">
                         Home
                         <i className=""></i> {/* Font Awesome icon */}
-                    </a>
+                    </Link>
                 </li>
                 <li className="nav-item dropdown">
-                    <a href="#">
+                    <Link to="#">
                         Topics
                         <i className="fas fa-chevron-up icon"></i> {/* Font Awesome icon */}
-                    </a>
+                    </Link>
                     <ul className="dropdown-content">
-                        <li><a href="#">Topic 1</a></li>
-                        <li><a href="#">Topic 2</a></li>
-                        <li><a href="#">Topic 3</a></li>
+                        <li><Link to="#">Topic 1</Link></li>
+                        <li><Link to="#">Topic 2</Link></li>
+                        <li><Link to="#">Topic 3</Link></li>
                     </ul>
                 </li>
                 <li className="nav-item">
-                    <a href="#">
+                    <Link to="#">
                         Reminder
                         <i className=""></i> {/* Font Awesome icon */}
-                    </a>
+                    </Link>
                 </li>
             </ul>
             <div className="search-bar">
                 <input type="text" placeholder="Search..." />
             </div>
             <div className="icons">
-                <a href="#"><i className="fa-solid fa-right-from-bracket"></i></a>
-                <a href="#"><i className="fas fa-user"></i></a>
+                <Link to="#"><i className="fa-solid fa-right-from-bracket"></i></Link>
+                <Link to="#"><i className="fas fa-user"></i></Link>
             </div>
         </nav>
     );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,35 @@
-
+import React from 'react';
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import Card from './components/Card/Card.jsx';
+import Reminder from './components/Reminder/Reminder.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                path: '',
+                element: <Card />
+            },
+            {
+                path:'reminder',
+                element: <Reminder />
+            }
+        ]
+    }
+])
 
 createRoot(document.getElementById('root')).render(
-
-    <App />
+ <React.StrictMode>
+  <RouterProvider router={router} />
+    
+ </React.StrictMode>
+    
 
 )
