4f3cb48f76a83e6a2b7afee25ba822491f8d820e
AdityaSrivastav5 <adityasrivastav863@gmail.com>
2025-05-09 23:49:05 +0530

    Fixes

--- backend/controller/grind75Controller.js
+++ backend/controller/grind75Controller.js
@@ -2,8 +2,10 @@ const Grind75Question = require('../models/grind75Model.js');
 
 // Make sure this is a function
 const getAllGrind75Questions = async (req, res) => {
+  console.log("hello from grind75")
   try {
     const questions = await Grind75Question.find({});
+    console.log(questions)
     res.status(200).json(questions);
   } catch (error) {
     res.status(500).json({ message: error.message });
