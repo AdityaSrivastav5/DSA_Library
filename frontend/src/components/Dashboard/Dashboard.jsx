1cdc1ddb3322844d943ca1e20f69eba31e7445ce
AdityaSrivastav5 <adityasrivastav863@gmail.com>
2025-05-31 12:25:57 +0530

    Some Changes

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -289,7 +289,7 @@ const Dashboard = () => {
   const fetchUserStats = async () => {
     try {
       const response = await axios.get(
-        `https://dsa-library.onrender.com/user/grind75-questions/user/stats/${user.id}`
+        `https://dsa-library.onrender.com/stats/${user.id}`
       );
       setUserStats(response.data.stats);
       setLoading(false);
